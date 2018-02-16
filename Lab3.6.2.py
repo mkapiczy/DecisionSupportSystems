3edf67d0f3ba937e352d24d4732b04948698c4b5
Lasse Have Nielsen <lasse@havenielsen.dk>
2018-02-16 11:08:50 +0100

    Lab 3.6.2

--- Lab3.6.2.py
+++ Lab3.6.2.py
@@ -1,17 +1,29 @@
-import scipy
-print('scipy: %s' % scipy.__version__)
-# numpy
-import numpy
-print('numpy: %s' % numpy.__version__)
-# matplotlib
-import matplotlib
-print('matplotlib: %s' % matplotlib.__version__)
-# pandas
-import pandas
-print('pandas: %s' % pandas.__version__)
-# statsmodels
-import statsmodels
-print('statsmodels: %s' % statsmodels.__version__)
-# scikit-learn
-import sklearn
-print('sklearn: %s' % sklearn.__version__)
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+
+data = np.loadtxt("Boston.data")
+
+xData = []
+yData = []
+for item in data:
+    xData.append([item[12]]) # The LSTAT Column
+    yData.append(item[13]) # The MEDV Column
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+regr.fit(xData, yData)
+
+# Printing relevant values
+print('Coefficients:' , regr.coef_)
+print("Mean squared error: %.2f" % np.mean((regr.predict(xData) - yData) ** 2))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(xData, yData))
+
+
+# Plot outputs
+plt.scatter(xData, yData,  color='black')
+plt.plot(xData, regr.predict(xData), color='blue', linewidth=3)
+plt.xlabel("LSTAT")
+plt.ylabel("MEDV")
+plt.show()
