bcaf434407234c112f6f2e83000b9baa25e46b62
Lasse Have Nielsen <lasse@havenielsen.dk>
2018-02-23 11:45:54 +0100

    4.6.2 is done.

--- Lab4.6.2.py
+++ Lab4.6.2.py
@@ -24,7 +24,66 @@ print(res.params)
 predictions = res.predict()
 print(predictions[0:10])
 
+# Convert probabilities to 'bools'
+# 1 is up like before and 0 is down
+predictions_nominal = [1 if x > 0.5 else 0 for x in predictions]
+
+from sklearn.metrics import confusion_matrix
+true_values = data.Direction
+conf_mat = confusion_matrix(true_values, predictions_nominal)
+print(conf_mat)
+
+# Probability for correct result
+res = (conf_mat[1,1]+conf_mat[0,0])/sum(sum(conf_mat))
+print(res)
+
+####  Test and training data ####
+# Select training data only
+# because it is sorted.
+train_data = data[0:sum(data.Year<2005)]
+test_data = data[sum(data.Year<2005):]
+
+# Create the train model
+res_train = smf.glm("Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume",data=train_data,family=sm.families.Binomial()).fit()
+# Summary of the trained model
+print(res_train.summary().tables[1])
+# Get the train models coefficients
+print(res_train.params)
+
+predictions = res_train.predict(test_data)
+predictions_nominal = [1 if x > 0.5 else 0 for x in predictions]
+
+true_values = test_data.Direction
+conf_mat_test = confusion_matrix(predictions_nominal, true_values)
+print(conf_mat_test)
+
+# Probability for correct result
+res_test = (conf_mat_test[1,1]+conf_mat_test[0,0])/sum(sum(conf_mat_test))
+print(res_test)
+
+####  NOW ONLY LAG 1 and 2 ####
+# Select training data only
+# because it is sorted.
+train_data = data[0:sum(data.Year<2005)]
+test_data = data[sum(data.Year<2005):]
+
+# Create the train model
+res_train = smf.glm("Direction ~ Lag1 + Lag2",data=train_data,family=sm.families.Binomial()).fit()
+# Summary of the trained model
+print(res_train.summary().tables[1])
+# Get the train models coefficients
+print(res_train.params)
+
+predictions = res_train.predict(test_data)
+predictions_nominal = [1 if x > 0.5 else 0 for x in predictions]
+
+true_values = test_data.Direction
+conf_mat_test = confusion_matrix(predictions_nominal, true_values)
+print(conf_mat_test)
+
+# Probability for correct result
+res_test = (conf_mat_test[1,1]+conf_mat_test[0,0])/sum(sum(conf_mat_test))
+print(res_test)
 
 
-
-# print(data.describe().transpose())
+print(res_train.predict(pd.DataFrame([[1.2, 1.1], [1.5, -0.8]], columns = ["Lag1", "Lag2"])))
