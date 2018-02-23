e91f470786de74361b48eef40b69318ac4acb645
Kåre Jensen <201370886@post.au.dk>
2018-02-23 10:25:22 +0100

    4.6.2

--- Lab4.6.2.py
+++ Lab4.6.2.py
@@ -1,3 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import linear_model
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
+data = pd.read_csv('Smarket.csv', usecols = range(1,10))
+
+for x in range(0, data.Direction.size):
+  if data.Direction[x].lower() in ['up']: 
+    data.loc[x, 'Direction'] = 1
+  else:
+    data.loc[x, 'Direction'] = 0
+
+res = smf.glm("Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume",data,family=sm.families.Binomial()).fit()
+print(res.summary().tables[1])
+
+# print(data.describe().transpose())
