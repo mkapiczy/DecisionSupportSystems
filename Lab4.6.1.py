33ec5b4dfa37e930a3c8e8e492bc77250bbdb47c
Kåre Jensen <201370886@post.au.dk>
2018-02-23 09:00:11 +0100

    Added lab 4.6.2

--- Lab4.6.1.py
+++ Lab4.6.1.py
@@ -1,20 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
-import seaborn as sns
-from statsmodels.graphics.gofplots import ProbPlot
-from sklearn.preprocessing import normalize
-import math
 
 data = pd.read_csv('Smarket.csv', index_col=0, parse_dates = True)
 summary = data.describe()
 
-# summary = summary.transpose()
-# print (summary)
+summary = summary.transpose()
+print (summary)
 
-# corr = data.corr()
-# print (corr)
+corr = data.corr()
+print (corr)
 
 plt.figure(1)
 plt.plot(data.Volume, 'bo', markersize = 3)
