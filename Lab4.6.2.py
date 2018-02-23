5112c777903b4b94966a2652218204acd662b422
Lasse Have Nielsen <lasse@havenielsen.dk>
2018-02-23 10:36:48 +0100

    added billions to 4.6.1 and predictions in 4.6.2

--- Lab4.6.2.py
+++ Lab4.6.2.py
@@ -6,6 +6,7 @@ import statsmodels.formula.api as smf
 
 data = pd.read_csv('Smarket.csv', usecols = range(1,10))
 
+# To get the values to fit the answer
 for x in range(0, data.Direction.size):
   if data.Direction[x].lower() in ['up']: 
     data.loc[x, 'Direction'] = 1
@@ -13,6 +14,17 @@ for x in range(0, data.Direction.size):
     data.loc[x, 'Direction'] = 0
 
 res = smf.glm("Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume",data,family=sm.families.Binomial()).fit()
+
+# Summary of the model
 print(res.summary().tables[1])
+# Get the coefficients
+print(res.params)
+
+# Get the first 10 predictions 
+predictions = res.predict()
+print(predictions[0:10])
+
+
+
 
 # print(data.describe().transpose())
