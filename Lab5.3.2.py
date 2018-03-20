c3e5a7c5e2bb2e525a410e62e380a8335e10480b
Lasse Have Nielsen <lhn@idealdev.dk>
2018-03-20 18:59:24 +0100

    plotted MSE as function of polynomial degree and wrote some report to it

--- Lab5.3.2.py
+++ Lab5.3.2.py
@@ -2,6 +2,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import LeaveOneOut, cross_val_score
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
@@ -19,6 +20,9 @@ print('Coefficients:', regr.coef_)
 #Leave One Out cross validation strategy
 loo = LeaveOneOut()
 
+scores = list()
+xplot = list()
+
 # Fit, predict and calculate mean squared error for polynomial regression for order degree 1-5
 for i in range(0,5):
       poly = PolynomialFeatures(degree=i+1)
@@ -27,3 +31,11 @@ for i in range(0,5):
       #Score using leave one out as split strategy
       score = cross_val_score(model, x_current, y, cv=loo, scoring="neg_mean_squared_error")
       print ('MSE for order:', i+1, score.mean())
+      scores.append(round(-score.mean(),2))
+      xplot.append(i+1)
+
+plt.tight_layout()
+plt.plot(xplot, scores, '-o')
+plt.xlabel('Polynomial Degree') 
+plt.ylabel ('Mean Squared Error')
+plt.savefig('LOOCV_532.png', dpi=200)
--- Lab5.3.3.py
+++ Lab5.3.3.py
@@ -4,6 +4,7 @@ warnings.simplefilter(action='ignore', category=RuntimeWarning)
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 from sklearn.model_selection import LeaveOneOut, cross_val_score, KFold
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
@@ -18,8 +19,10 @@ regr.fit(x, y)
 
 print("Coeffecients: %.2f" % regr.coef_)
 #K fold cross validation strategy
-knn = KFold(n_splits=x.shape[0])
+knn = KFold(n_splits=10)
 
+scores = list()
+xplot = list()
 # Fit, predict and calculate mean squared error for polynomial regression for order degree 1-10
 for i in range(0,10):
       poly = PolynomialFeatures(degree=i+1)
@@ -28,3 +31,11 @@ for i in range(0,10):
       #Score using leave one out as split strategy
       score = cross_val_score(model, x_current, y, cv=knn, scoring="neg_mean_squared_error")
       print ('MSE for order:', i+1, score.mean())
+      scores.append(round(-score.mean(),2))
+      xplot.append(i+1)
+
+plt.tight_layout()
+plt.plot(xplot, scores, '-o')
+plt.xlabel('Polynomial Degree') 
+plt.ylabel ('Mean Squared Error')
+plt.savefig('KFOLDCV_533.png', dpi=200)
