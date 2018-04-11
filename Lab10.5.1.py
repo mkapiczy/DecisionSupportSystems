2cfd4b07f24b690b027de81c5b17f28541ae3c8c
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2018-04-11 15:23:13 +0200

    Lab10.5.1 done

--- Lab10.5.1.py
+++ Lab10.5.1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Create random variable with x,y coordinates
-np.random.seed(321)
+np.random.seed(123)
 X = np.random.randn(50,2)
 
 # Seperate data into two equally large groups 
@@ -11,24 +11,31 @@ X[0:25,0] = X[0:25,0]+4
 X[0:25,1] = X[0:25,1]-4
 
 # Plot the data
-fig, ax = plt.subplots(figsize=(6, 5))
-ax.scatter(X[:, 0], X[:, 1], s=50)
-ax.set_xlabel('X0')
-ax.set_ylabel('X1')
-
-kmeans = KMeans(n_clusters=2, random_state=123).fit(X)
-
-print(kmeans.labels_)
-
-plt.figure(figsize=(6,5))
-plt.scatter(X[:,0], X[:,1], s = 50, c = kmeans.labels_, cmap = plt.cm.bwr)
-plt.scatter(kmeans.cluster_centers_[:, 0],
-    kmeans.cluster_centers_[:, 1],
-    marker = '^',
-    s = 150,
-    color = 'black',
-    label = 'Centers')
-plt.legend(loc = 'best')
+plt.figure(figsize=(6, 5))
+plt.scatter(X[:, 0], X[:, 1], s=50)
 plt.xlabel('X0')
 plt.ylabel('X1')
+plt.title('Plot of original data points')
+
+# Try k-means algorithm with different number of clusters and different initial starting points for each centroid. 
+for k in 2,3:
+    for n in 1,20:
+        kmeans = KMeans(n_clusters=k, n_init=n, random_state=11).fit(X)
+        print(kmeans.labels_)
+
+        # Plot the cluster assignments
+        plt.figure(figsize=(6,5))
+        plt.scatter(X[:, 0], X[:, 1], s = 50, c = kmeans.labels_)
+        plt.scatter(kmeans.cluster_centers_[:, 0],
+            kmeans.cluster_centers_[:, 1],
+            marker = '^',
+            s = 100,
+            color = 'black',
+            label = 'Centers')
+        plt.legend(loc = 'best')
+        plt.xlabel('X0')
+        plt.ylabel('X1')
+        plt.title('Cluster assignment with: K=%i and n=%i' %(k,n))
+        print('K=', k, 'n=', n, 'WCV=', kmeans.inertia_)
+        
 plt.show()
